423265b86ff9237bef4045bb6fe6fc6fc2e846b0
Maynor <maynora31@gmail.com>
2023-09-19 11:51:55 +0200

    Code cleanup

--- src/app/modules/users/pages/add-edit-user/add-edit-user.component.ts
+++ src/app/modules/users/pages/add-edit-user/add-edit-user.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {
-  FormBuilder,
-  Validators,
-  FormGroup,
-  FormControl,
   AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
 } from '@angular/forms';
-import { UserService } from '../../services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-add-edit-user',
--- src/app/modules/users/services/user.service.ts
+++ src/app/modules/users/services/user.service.ts
@@ -1,8 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../models/user.model';
 import { environment } from 'src/enviroments/enviroment';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
